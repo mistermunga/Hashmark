686b1209a465f4076c61b5b18d1e53d97998548e
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2026-02-20 13:33:32 +0300

    initial commit

---
+++ src/main/java/xyz/ryansbeanfactory/hashmark/hashmark/HashmarkApplication.java
@@ -0,0 +1,13 @@
+package xyz.ryansbeanfactory.hashmark.hashmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashmarkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HashmarkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/ryansbeanfactory/hashmark/hashmark/HashmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.ryansbeanfactory.hashmark.hashmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HashmarkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
