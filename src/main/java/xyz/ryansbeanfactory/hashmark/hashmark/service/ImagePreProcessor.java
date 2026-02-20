76ec794f93aadff7b3cf422e610c941b70a4a4a5
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2026-02-20 14:30:08 +0300

    completed the preprocessor

--- src/main/java/xyz/ryansbeanfactory/hashmark/hashmark/service/ImagePreProcessor.java
+++ src/main/java/xyz/ryansbeanfactory/hashmark/hashmark/service/ImagePreProcessor.java
@@ -2,15 +2,26 @@ package xyz.ryansbeanfactory.hashmark.hashmark.service;
 
 import org.springframework.stereotype.Service;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Service
 public class ImagePreProcessor {
 
-    public BufferedImage preprocess(BufferedImage original) {}
+    private static Path OUTPUT_FOLDER;
+    private final String ROOT_FOLDER;
 
-    private BufferedImage canonicalizeToPNG(BufferedImage original) {
+    public ImagePreProcessor() {
+        ROOT_FOLDER = System.getProperty("user.dir");
+        OUTPUT_FOLDER = Paths.get(ROOT_FOLDER, "output");
+    }
+
+    public BufferedImage canonicalizeToPNG(BufferedImage original) {
         BufferedImage canonical = new BufferedImage(
                 original.getWidth(),
                 original.getHeight(),
@@ -24,5 +35,12 @@ public class ImagePreProcessor {
         return canonical;
     }
 
-    private BufferedImage save() {}
+    public String save(BufferedImage image, String imageName) throws IOException {
+        Path imagePath = OUTPUT_FOLDER.resolve(imageName + ".png");
+        Files.createDirectories(OUTPUT_FOLDER);
+        ImageIO.write(image, "png", imagePath.toFile());
+        return Paths.get(ROOT_FOLDER)
+                .relativize(imagePath)
+                .toString();
+    }
 }
